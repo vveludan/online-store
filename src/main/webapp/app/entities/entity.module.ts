3bc347c798cabead3f71c8fb66db29a484f492e9
Venkat Veludandi <vveludan@gmail.com>
2019-06-20 14:32:11 -0400

    generated online store entity model

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,34 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'product',
+        loadChildren: './product/product.module#StoreProductModule'
+      },
+      {
+        path: 'product-category',
+        loadChildren: './product-category/product-category.module#StoreProductCategoryModule'
+      },
+      {
+        path: 'customer',
+        loadChildren: './customer/customer.module#StoreCustomerModule'
+      },
+      {
+        path: 'product-order',
+        loadChildren: './product-order/product-order.module#StoreProductOrderModule'
+      },
+      {
+        path: 'order-item',
+        loadChildren: './order-item/order-item.module#StoreOrderItemModule'
+      },
+      {
+        path: 'invoice',
+        loadChildren: './invoice/invoice.module#StoreInvoiceModule'
+      },
+      {
+        path: 'shipment',
+        loadChildren: './shipment/shipment.module#StoreShipmentModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
